30c81d27d5c69fc7154be2a8f8c34764fd4ef06a
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2023-07-05 10:33:17 +0200

    Create User.java
    class for creating Users

---
+++ backend/src/main/backendMatvey/User.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class User {
+    private String first_name;
+    private String last_name;
+    private String user_name;
+
+    public User(String first_name, String last_name, String user_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.user_name = user_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+}
