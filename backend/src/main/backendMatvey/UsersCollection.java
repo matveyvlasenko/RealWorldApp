5b25648c782a9c418fced830bfe43928b27c3fda
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2023-07-05 11:19:12 +0200

    Create UsersCollection.java

---
+++ backend/src/main/backendMatvey/UsersCollection.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersCollection {
+    List<User> userList = new ArrayList<User>();
+
+
+    public void addUser( User user){
+        userList.add(user);
+    }
+
+    public void removeUser(User user){
+        userList.remove(user);
+    }
+    public void removeAll(){
+        userList.removeAll(userList);
+    }
+    public User getUser(User user){
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersCollection{" +
+                "userList=" + userList +
+                '}';
+    }
+}
